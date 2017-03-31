225dd63d2a90276f1d883e304b74bc6c578c7e39
Willson Smith <me@willsonsmith.com>
2017-03-30 23:28:05 -0400

    more posts button, some cleanup

--- src/ui/components/reddit-feed/component.ts
+++ src/ui/components/reddit-feed/component.ts
@@ -1,18 +1,18 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class RedditFeed extends Component {
-  @tracked subreddit;
   @tracked posts;
+  afterMarker = "";
   
   constructor(options) {
     super(options);
-    this.getPosts(this.args.subreddit);
+    this.getPosts();
   }
 
-  async getPosts(subreddit) {
-    let response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
+  async getPosts() {
+    let response = await fetch(`https://www.reddit.com/r/${this.args.subreddit}.json?after=${this.afterMarker}`);
     let postData = await response.json();
+    this.afterMarker = postData.data.after;
     this.posts = postData.data.children.map((post) => post.data );
-    console.log(this.posts);
   }
 };
