cee507e32377dddb205340c36d0b5372d696010c
Willson Smith <me@willsonsmith.com>
2017-03-31 23:08:46 -0400

    strip 'default' from thumbnails, return object with exists or not for thumbnail. some styles cleaned up

--- src/ui/components/reddit-feed/component.ts
+++ src/ui/components/reddit-feed/component.ts
@@ -1,5 +1,14 @@
 import Component, { tracked } from '@glimmer/component';
 
+function stripDefaultThumbnails(post) {
+  if (post.thumbnail && post.thumbnail === 'default') {
+    post.thumbnail = {exists: false};
+  } else {
+    post.thumbnail = {exists: true, thumbnail: post.thumbnail};
+  }
+  return post;
+}
+
 export default class RedditFeed extends Component {
   @tracked posts;
   afterMarker = "";
@@ -13,7 +22,7 @@ export default class RedditFeed extends Component {
     let response = await fetch(`https://www.reddit.com/r/${this.args.subreddit}.json?after=${this.afterMarker}`);
     let postData = await response.json();
     this.afterMarker = postData.data.after;
-    this.posts = postData.data.children.map((post) => post.data );
+    this.posts = postData.data.children.map((post) => stripDefaultThumbnails(post.data) );
     document.querySelector('a').focus();
   }
 };
